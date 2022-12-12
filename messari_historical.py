8744f1823d6e3205837ca35a0b84a33d8f3799e1
ncv18 <77590102+ncv18@users.noreply.github.com>
2022-12-12 21:53:32 +0100

    Update messari_historical.py

--- messari_historical.py
+++ messari_historical.py
@@ -13,8 +13,10 @@ import numpy
 #list of metrics
 metric_ids =  ['act.addr.cnt', 'addr.act.rcv.cnt', 'addr.act.sent.cnt', 'addr.bal.0.001.ntv.cnt', 'addr.bal.0.01.ntv.cnt', 'addr.bal.0.1.ntv.cnt', 'addr.bal.1.cnt', 'addr.bal.1.ntv.cnt', 'addr.bal.10.cnt', 'addr.bal.10.ntv.cnt', 'addr.bal.100.cnt', 'addr.bal.100.ntv.cnt', 'addr.bal.100k.cnt', 'addr.bal.100k.ntv.cnt', 'addr.bal.10k.cnt', 'addr.bal.10k.ntv.cnt', 'addr.bal.10m.cnt', 'addr.bal.1k.cnt', 'addr.bal.1k.ntv.cnt', 'addr.bal.1m.cnt', 'addr.bal.1m.ntv.cnt', 'addr.cnt', 'blk.cnt', 'blk.gas.limit.avg', 'blk.hgt', 'blk.int.avg', 'blk.size.byte', 'blk.size.bytes.avg', 'blk.unc.cnt', 'blk.unc.rew', 'blk.unc.rew.ntv', 'blk.unc.rew.pct', 'blk.wght.avg', 'blk.wght.total', 'daily.shp', 'daily.vol', 'diff.avg', 'fees', 'fees.ntv', 'fees.pct.rev', 'hash.rev', 'hash.rev.ntv', 'hashrate', 'hashrate.30d', 'hashrate.rev', 'hashrate.rev.ntv', 'iss.rate', 'iss.rate.day', 'mcap.circ', 'mcap.dom', 'mcap.out', 'mcap.realized', 'min.rev.ntv', 'min.rev.usd', 'miner.1hop.sply', 'miner.1hop.sply.ntv', 'miner.rev.total', 'miner.sply', 'miner.sply.ntv', 'new.iss.ntv', 'new.iss.usd', 'nvt.adj', 'nvt.adj.90d.ma', 'price', 'real.vol', 'reddit.active.users', 'reddit.subscribers', 'rvt.adj', 'rvt.adj.90d.ma', 'sply.act.10y', 'sply.act.180d', 'sply.act.1d', 'sply.act.1y', 'sply.act.2y', 'sply.act.30d', 'sply.act.3y', 'sply.act.4y', 'sply.act.5y', 'sply.act.7d', 'sply.act.90d', 'sply.act.evr', 'sply.act.pct.1y', 'sply.addr.bal.0.001.ntv', 'sply.addr.bal.0.01.ntv', 'sply.addr.bal.0.1.ntv', 'sply.addr.bal.1', 'sply.addr.bal.1.ntv', 'sply.addr.bal.10', 'sply.addr.bal.10.ntv', 'sply.addr.bal.100', 'sply.addr.bal.100.ntv', 'sply.addr.bal.100k', 'sply.addr.bal.100k.ntv', 'sply.addr.bal.10k', 'sply.addr.bal.10k.ntv', 'sply.addr.bal.10m', 'sply.addr.bal.1k', 'sply.addr.bal.1k.ntv', 'sply.addr.bal.1m', 'sply.addr.bal.1m.ntv', 'sply.circ', 'sply.cont.ntv', 'sply.cont.usd', 'sply.out', 'sply.rvv.180d', 'sply.rvv.1y', 'sply.rvv.2y', 'sply.rvv.30d', 'sply.rvv.3y', 'sply.rvv.4y', 'sply.rvv.5y', 'sply.rvv.7d', 'sply.rvv.90d', 'sply.shld', 'sply.top.100', 'sply.top.10pct', 'sply.top.1pct', 'sply.total.iss', 'sply.total.iss.ntv', 'sply.utxo.loss', 'sply.utxo.prof', 'telegram.users', 'twitter.followers', 'txn.cnt', 'txn.cnt.sec', 'txn.cont.call.cnt', 'txn.cont.call.succ.cnt', 'txn.cont.cnt', 'txn.cont.creat.cnt', 'txn.cont.dest.cnt', 'txn.erc20.cnt', 'txn.erc721.cnt', 'txn.fee.avg', 'txn.fee.avg.ntv', 'txn.fee.med', 'txn.fee.med.ntv', 'txn.gas', 'txn.gas.avg', 'txn.gas.limit', 'txn.gas.limit.avg', 'txn.tfr.avg.ntv', 'txn.tfr.erc20.cnt', 'txn.tfr.erc721.cnt', 'txn.tfr.val.adj.ntv', 'txn.tfr.val.med', 'txn.tfr.val.med.ntv', 'txn.tfr.val.ntv', 'txn.tkn.cnt', 'txn.tsfr.cnt', 'txn.tsfr.val.adj', 'txn.tsfr.val.avg', 'txn.vol', 'utxo.age.avg', 'utxo.age.med', 'utxo.age.val.avg', 'utxo.cnt', 'utxo.loss.cnt', 'utxo.prof.cnt']
 
-result = numpy.empty((940,168))
+result = numpy.empty((940,168)) #change number of rows according to size of data by dates, columns = number of metrics
 result[:] = numpy.nan
+
+#take initial date as number
 day_zero =  1588291200/86400 # 1st may 2020
 #day_zero = 1493596800/86400 #1st may 2017
 #day_zero = 1525132800/86400 #1st may 2018
@@ -40,6 +42,8 @@ def  getdataf(metrics):
     return schema, series
 
 def main():
+    #data indexed into result array according to date
+    
     k = 0
     for metric in metric_ids:
         (variables, vals) = getdataf(metric)
@@ -47,7 +51,7 @@ def main():
             k = k + 1
             continue
         a = numpy.array(vals)
-        days = (a[:,0])/86400000
+        days = (a[:,0])/86400000 
         rows = len(days)
         column = len(a[0])
         if column != 2:
@@ -63,7 +67,7 @@ def main():
             k += 1
             
     df = pd.DataFrame(result, columns = ['act.addr.cnt', 'addr.act.rcv.cnt', 'addr.act.sent.cnt', 'addr.bal.0.001.ntv.cnt', 'addr.bal.0.01.ntv.cnt', 'addr.bal.0.1.ntv.cnt', 'addr.bal.1.cnt', 'addr.bal.1.ntv.cnt', 'addr.bal.10.cnt', 'addr.bal.10.ntv.cnt', 'addr.bal.100.cnt', 'addr.bal.100.ntv.cnt', 'addr.bal.100k.cnt', 'addr.bal.100k.ntv.cnt', 'addr.bal.10k.cnt', 'addr.bal.10k.ntv.cnt', 'addr.bal.10m.cnt', 'addr.bal.1k.cnt', 'addr.bal.1k.ntv.cnt', 'addr.bal.1m.cnt', 'addr.bal.1m.ntv.cnt', 'addr.cnt', 'blk.cnt', 'blk.gas.limit.avg', 'blk.hgt', 'blk.int.avg', 'blk.size.byte', 'blk.size.bytes.avg', 'blk.unc.cnt', 'blk.unc.rew', 'blk.unc.rew.ntv', 'blk.unc.rew.pct', 'blk.wght.avg', 'blk.wght.total', 'sharpe_30d', 'sharpe_90d','sharpe_1yr','sharpe_3yr','volatility_30d', 'volatility_90d', 'volatility_1yr', 'volatility_3yr','diff.avg', 'fees', 'fees.ntv', 'fees.pct.rev', 'hash.rev', 'hash.rev.ntv', 'hashrate', 'hashrate.30d', 'hashrate.rev', 'hashrate.rev.ntv', 'iss.rate', 'iss.rate.day', 'mcap.circ', 'mcap.dom', 'mcap.out', 'mcap.realized', 'min.rev.ntv', 'min.rev.usd', 'miner.1hop.sply', 'miner.1hop.sply.ntv', 'miner.rev.total', 'miner.sply', 'miner.sply.ntv', 'new.iss.ntv', 'new.iss.usd', 'nvt.adj', 'nvt.adj.90d.ma', 'open', 'high', 'low', 'close', 'volume', 'real.vol', 'reddit.active.users', 'reddit.subscribers', 'rvt.adj', 'rvt.adj.90d.ma', 'sply.act.10y', 'sply.act.180d', 'sply.act.1d', 'sply.act.1y', 'sply.act.2y', 'sply.act.30d', 'sply.act.3y', 'sply.act.4y', 'sply.act.5y', 'sply.act.7d', 'sply.act.90d', 'sply.act.evr', 'sply.act.pct.1y', 'sply.addr.bal.0.001.ntv', 'sply.addr.bal.0.01.ntv', 'sply.addr.bal.0.1.ntv', 'sply.addr.bal.1', 'sply.addr.bal.1.ntv', 'sply.addr.bal.10', 'sply.addr.bal.10.ntv', 'sply.addr.bal.100', 'sply.addr.bal.100.ntv', 'sply.addr.bal.100k', 'sply.addr.bal.100k.ntv', 'sply.addr.bal.10k', 'sply.addr.bal.10k.ntv', 'sply.addr.bal.10m', 'sply.addr.bal.1k', 'sply.addr.bal.1k.ntv', 'sply.addr.bal.1m', 'sply.addr.bal.1m.ntv', 'sply.circ', 'sply.cont.ntv', 'sply.cont.usd', 'sply.out', 'sply.rvv.180d', 'sply.rvv.1y', 'sply.rvv.2y', 'sply.rvv.30d', 'sply.rvv.3y', 'sply.rvv.4y', 'sply.rvv.5y', 'sply.rvv.7d', 'sply.rvv.90d', 'sply.shld', 'sply.top.100', 'sply.top.10pct', 'sply.top.1pct', 'sply.total.iss', 'sply.total.iss.ntv', 'sply.utxo.loss', 'sply.utxo.prof', 'telegram.users', 'twitter.followers', 'txn.cnt', 'txn.cnt.sec', 'txn.cont.call.cnt', 'txn.cont.call.succ.cnt', 'txn.cont.cnt', 'txn.cont.creat.cnt', 'txn.cont.dest.cnt', 'txn.erc20.cnt', 'txn.erc721.cnt', 'txn.fee.avg', 'txn.fee.avg.ntv', 'txn.fee.med', 'txn.fee.med.ntv', 'txn.gas', 'txn.gas.avg', 'txn.gas.limit', 'txn.gas.limit.avg', 'txn.tfr.avg.ntv', 'txn.tfr.erc20.cnt', 'txn.tfr.erc721.cnt', 'txn.tfr.val.adj.ntv', 'txn.tfr.val.med', 'txn.tfr.val.med.ntv', 'txn.tfr.val.ntv', 'txn.tkn.cnt', 'txn.tsfr.cnt', 'txn.tsfr.val.adj', 'txn.tsfr.val.avg', 'txn.vol', 'utxo.age.avg', 'utxo.age.med', 'utxo.age.val.avg', 'utxo.cnt', 'utxo.loss.cnt', 'utxo.prof.cnt'])         
-    df.to_excel (r'directory\file_name.xlsx', header=True)
+    df.to_excel (r'directory\file_name.xlsx', header=True) #specify directory and file name.
 if __name__ == "__main__":
     main()
     
